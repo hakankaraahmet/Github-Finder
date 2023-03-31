1d68a87900e5a80275e417e2140b35c24d3508ce
hakan karaahmetoglu <ext1482@foss.dk>
2023-03-31 15:35:18 +0300

    start icon and fonts are  changed

--- src/components/MainTemplate.tsx
+++ src/components/MainTemplate.tsx
@@ -9,37 +9,32 @@ const MainTemplate = () => {
   const [user, setUser] = useState<string>("");
   const dispatch = useDispatch();
 
-  useEffect(() => {
+  const handleSearch = () => {
     dispatch(fetchReposAction(user));
     dispatch(fetchProfileAction(user));
-  }, [dispatch, user]);
+  };
+
   const store = useSelector((state: any) => state?.repos);
   const { loading, reposList, profileList, error } = store;
 
-  console.log("user", user)
-
-
   return (
-    <div>
+    <div className="min-h-screen border border-gray-800 bg-gray-800">
       {" "}
-      <section className="relative 2xl bg-gray-800 min-h-screen">
+      <section className="relative 2xl ">
         <div className="relative container px-4 mx-auto">
           <div className="text-center mb-20">
-            {/* <span className="text-lg text-blue-400 font-bold">
-          Have a question?
-        </span> */}
             <div className="flex justify-center">
               <img
                 className="h-64  rounded-lg object-cover"
-                src="/gith.svg"
+                src="/github-mark-white.png"
                 alt=""
               />
             </div>
 
-            <h2 className="mt-10 mb-5 text-5xl font-bold font-heading text-indigo-300">
-              GitHub Finder
+            <h2 className="mt-16 mb-5 text-5xl font-bold font-heading text-white">
+              GitHub Profile Finder
             </h2>
-            <div className="mt-1 flex justify-center">
+            <div className="mt-8 flex justify-center">
               {/* input */}
               <input
                 value={user}
@@ -50,13 +45,23 @@ const MainTemplate = () => {
                 className="shadow-sm text-center focus:ring-indigo-500 p-2 focus:border-indigo-500  sm:text-sm border-gray-300 w-full rounded-md lg:w-1/2"
                 placeholder="Search For User"
               />
+              <button
+                className="border border-white rounded-xl text-white ml-4 px-10 "
+                onClick={handleSearch}
+              >
+                Search
+              </button>
             </div>
           </div>
           {/* Content goes here */}
           {loading ? (
-            <h1 className="text-green-300 text-3xl text-center">Loading please wait...</h1>
+            <h1 className="text-green-300 text-3xl text-center">
+              Loading please wait...
+            </h1>
           ) : error ? (
-            <h2 className="text-red-300 text-3xl text-center">{error?.data.message}</h2>
+            <h2 className="text-red-300 text-3xl text-center">
+              {error?.data.message}
+            </h2>
           ) : (
             <div className="max-w-4xl mx-auto">
               <div className="flex flex-wrap -mx-4 mb-20">
@@ -64,83 +69,85 @@ const MainTemplate = () => {
                   <div className="bg-gray-600 rounded-b-lg">
                     <div className="flex justify-center">
                       <img
-                  className="w-56 h-56 rounded-full"
-                  src={profileList?.avatar_url}
-                  alt=""
-                />
+                        className="w-56 h-56 rounded-full mt-8"
+                        src={profileList?.avatar_url}
+                        alt=""
+                      />
                     </div>
                     <div className="px-14 py-8">
                       <div className="mb-6 py-px bg-gray-500"></div>
-                      <h4 className="mb-8 lg:mb-4  text-white font-bold">
+                      <h4 className="mb-8 lg:mb-4  text-gray-900 font-bold">
                         Name:{" "}
-                  <span>
-                    {profileList?.name} {profileList?.login}
-                  </span>
+                        <span className="text-white">
+                          {profileList?.name} {profileList?.login}
+                        </span>
                       </h4>
                       <div className="mb-6 py-px bg-gray-500"></div>
-                      <h4 className="mb-8 lg:mb-4  text-white font-bold">
+                      <h4 className="mb-8 lg:mb-4  text-gray-900 font-bold">
                         {/* Bio goes here */}
                         Bio:{" "}
-                        <span>
-                    {profileList?.bio} 
-                  </span>
+                        <span className="text-white">{profileList?.bio}</span>
                       </h4>
                       <div className="mb-6 py-px bg-gray-500"></div>
-                      <h4 className="mb-8 lg:mb-4  text-white font-bold">
+                      <h4 className="mb-8 lg:mb-4  text-gray-900 font-bold">
                         {/* Company goes here */}
                         Company:{" "}
-                        <span>
-                    {profileList?.company} 
-                  </span>
+                        <span className="text-white">
+                          {profileList?.company}
+                        </span>
                       </h4>
                       <div className="mb-6 py-px bg-gray-500"></div>
-                      <h4 className="mb-8 lg:mb-4  text-white font-bold">
+                      <h4 className="mb-8 lg:mb-4  text-gray-900 font-bold">
                         {/* Location goes here */}
                         Location:{" "}
-                        <span>
-                    {profileList?.location} 
-                  </span>
+                        <span className="text-white">
+                          {profileList?.location}
+                        </span>
                       </h4>
 
                       <div className="mb-6 py-px bg-gray-500"></div>
-                      <h4 className="mb-8 lg:mb-4  text-white font-bold">
+                      <h4 className="mb-8 lg:mb-4  text-gray-900 font-bold">
                         {/* followers goes here */}
                         Followers:{" "}
-                        <span>
-                    {profileList?.followers} 
-                  </span>
+                        <span className="text-white">
+                          {profileList?.followers}
+                        </span>
                       </h4>
 
                       <div className="mb-6 py-px bg-gray-500"></div>
-                      <h4 className="mb-8 lg:mb-4  text-white font-bold">
+                      <h4 className="mb-8 lg:mb-4  text-gray-900 font-bold">
                         {/* following goes here */}
                         Following:{" "}
-                        <span>
-                    {profileList?.following} 
-                  </span>
+                        <span className="text-white">
+                          {profileList?.following}
+                        </span>
                       </h4>
 
                       <div className="mb-6 py-px bg-gray-500"></div>
-                      <h4 className="mb-8 lg:mb-4  text-white font-bold">
+                      <h4 className="mb-8 lg:mb-4  text-gray-900 font-bold">
                         Repositories:{" "}
-                  <span className="ml-2 inline-flex items-center px-2.5 py-0.5 rounded-lg text-xs font-medium bg-green-500 text-green-800">
-                    {profileList?.public_repos ? profileList?.public_repos : "N/A"}
-                  </span>
+                        <span className="ml-2 inline-flex items-center px-2.5 py-0.5 rounded-lg text-xs font-medium bg-green-500 text-green-800">
+                          {profileList?.public_repos
+                            ? profileList?.public_repos
+                            : "N/A"}
+                        </span>
                       </h4>
 
                       <div className="mb-6 py-px bg-gray-500"></div>
-                      <h4 className="mb-8 lg:mb-4  text-white font-bold">
+                      <h4 className="mb-8 lg:mb-4  text-gray-900 font-bold">
                         Gists:{" "}
-                  <span className="ml-2 inline-flex items-center px-2.5 py-0.5 rounded-lg text-xs font-medium bg-green-500 text-green-800">
-                    {profileList?.public_gists ? profileList?.public_gists : "N/A"}
-                  </span>
+                        <span className="ml-2 inline-flex items-center px-2.5 py-0.5 rounded-lg text-xs font-medium bg-green-500 text-green-800">
+                          {profileList?.public_gists
+                            ? profileList?.public_gists
+                            : "N/A"}
+                        </span>
                       </h4>
                       <div className="mb-6 py-px bg-gray-500"></div>
                       <div className="md:text-right">
                         <a
-                        href= {profileList?.html_url}
+                          href={profileList?.html_url}
                           target="_blank"
-                          className="inline-block px-12 py-4 border border-gray-300 hover:border-gray-200 rounded-full font-bold text-white"
+                          className="inline-block px-12 py-4 border border-gray-300 hover:border-gray-200 rounded-full font-bold text-gray-900"
                         >
                           View Profile
                         </a>
@@ -152,53 +159,22 @@ const MainTemplate = () => {
                 <div className="w-full lg:w-1/2 px-4">
                   {/* Loop this */}
 
-                  {reposList?.name !== 'Error' && reposList?.map((repo:any) => (
-
-                  <>
-                    <div className="py-6 px-8 mb-4 bg-gray-600 rounded-lg">
-                      <div className="flex items-center">
-                        <a
-                          href={repo?.html_url}
-                          target="_blank"
-                          className="text-lg  text-indigo-400"
-                        >{repo?.name}</a>
-                      </div>
-                    </div>
-                  </> 
-                  ) )}
-
+                  {reposList?.name !== "Error" &&
+                    reposList?.map((repo: any) => (
+                      <a
+                        href={repo?.html_url}
+                        target="_blank"
+                        className="text-xl font-bold  text-gray-900"
+                        key={repo?.html_url}
+                      >
+                        <div className="py-6 px-8 mb-4 bg-gray-600 rounded-lg">
+                          <div className="flex items-center">{repo?.name}</div>
+                        </div>
+                      </a>
+                    ))}
 
                   <div className="py-6 px-8"></div>
                 </div>
-              </div>
-              <div className="text-center bg-gray-300 ">
-                <p className="mb-4  text-gray-300">
-                  Developed by
-                  <span className="p-2 text-yellow-300">
-                    <a href="https://www.youtube.com/channel/UCvu6J9q1AM6q4xysGqAvVyw">
-                      i-Novotek
-                    </a>
-                  </span>
-                </p>
-                <a
-                  className="inline-flex text-blue-400 hover:text-blue-500 font-bold"
-                  href="https://www.youtube.com/channel/UCvu6J9q1AM6q4xysGqAvVyw"
-                >
-                  <span className="mb-10">Watch the tutorial</span>
-                  <svg
-                    className="ml-4 w-4 h-5"
-                    width="19"
-                    height="20"
-                    viewBox="0 0 19 20"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M18.7383 1.47342L18.7383 10.9304L17.5562 10.9304L17.5562 2.89788L0.834948 19.625L0.00154682 18.7916L16.7228 2.06448L9.28125 2.06448L9.28125 0.882355L18.1472 0.882355C18.4737 0.882355 18.7383 1.14697 18.7383 1.47342Z"
-                      fill="#1F40FF"
-                    ></path>
-                  </svg>
-                </a>
               </div>
             </div>
           )}
